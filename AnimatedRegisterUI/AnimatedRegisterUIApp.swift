7691bf7c4b5a751341691b298392518e793cafef
onkar8281 <onkarvaidya146@gmail.com>
2026-03-29 22:28:40 +0530

    Initial Commit

---
+++ AnimatedRegisterUI/AnimatedRegisterUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedRegisterUIApp.swift
+//  AnimatedRegisterUI
+//
+//  Created by Onkar Vaidya on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedRegisterUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
